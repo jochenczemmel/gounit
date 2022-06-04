e9ca24130ea4059549fd86402f0a4fc81bfa328e
jochen <jochen.czemmel@web.de>
2022-06-04 20:42:40 +0200

    neue Funktion EqualList

--- assert/assert.go
+++ assert/assert.go
@@ -11,6 +11,22 @@ type ErrorHelper interface {
 func Equal[C comparable](e ErrorHelper, got, want C) {
 	e.Helper()
 	if got != want {
-		e.Errorf("ERROR: got \"%v\", want \"%v\"", got, want)
+		e.Errorf("ERROR: got: \"%v\", want: \"%v\"", got, want)
+	}
+}
+
+// EqualList checks if two lists are equal.
+// If the lenght is different, the elements are not compared.
+func EqualList[L ~[]C, C comparable](e ErrorHelper, got, want L) {
+	e.Helper()
+	if len(got) != len(want) {
+		e.Errorf("ERROR: length: got %d, want: %d",
+			len(got), len(want))
+		return
+	}
+	for i, w := range want {
+		if got[i] != w {
+			e.Errorf("ERROR: [%d] got: \"%v\", want: \"%v\"", i, got[i], w)
+		}
 	}
 }
--- assert/assert_test.go
+++ assert/assert_test.go
@@ -15,7 +15,11 @@ type TestSpy struct {
 
 func (t *TestSpy) Errorf(format string, args ...any) {
 	t.ErrorCalled = true
-	t.ErrorMessage = fmt.Sprintf(format, args...)
+	if t.ErrorMessage == "" {
+		t.ErrorMessage = fmt.Sprintf(format, args...)
+		return
+	}
+	t.ErrorMessage += "/" + fmt.Sprintf(format, args...)
 }
 
 func (t *TestSpy) Helper() {}
@@ -28,16 +32,16 @@ func TestEqual(t *testing.T) {
 		wantMessage    string
 	}{
 		{
-			name:      "equal",
-			value1:    "a",
-			value2:    "a",
-			wantError: false,
+			name:   "equal",
+			value1: "a",
+			value2: "a",
 		},
-		{name: "not equal",
+		{
+			name:        "not equal",
 			value1:      "a",
 			value2:      "b",
 			wantError:   true,
-			wantMessage: `ERROR: got "a", want "b"`,
+			wantMessage: `ERROR: got: "a", want: "b"`,
 		},
 	}
 	for _, c := range candidates {
@@ -49,7 +53,91 @@ func TestEqual(t *testing.T) {
 					t.Errorf("ERROR: error not detected")
 				}
 				if ts.ErrorMessage != c.wantMessage {
-					t.Errorf("ERROR: got: %s, want: %s",
+					t.Errorf("ERROR: got: \"%s\", want: \"%s\"",
+						ts.ErrorMessage, c.wantMessage)
+				}
+			}
+			if !c.wantError && ts.ErrorCalled {
+				t.Errorf("ERROR: false alarm")
+				t.Logf("NOTE: error message is: %v", ts.ErrorMessage)
+			}
+		})
+	}
+}
+
+func TestEqualList(t *testing.T) {
+
+	candidates := []struct {
+		name           string
+		value1, value2 []int
+		wantError      bool
+		wantMessage    string
+	}{
+		{
+			name:   "equal",
+			value1: []int{1, 2, 3},
+			value2: []int{1, 2, 3},
+		},
+		{
+			name:        "different length",
+			value1:      []int{1, 2, 3},
+			value2:      []int{1, 2},
+			wantError:   true,
+			wantMessage: `ERROR: length: got 3, want: 2`,
+		},
+		{
+			name:        "different value",
+			value1:      []int{1, 2, 3},
+			value2:      []int{1, 3, 3},
+			wantError:   true,
+			wantMessage: `ERROR: [1] got: "2", want: "3"`,
+		},
+		{
+			name:      "different values",
+			value1:    []int{1, 2, 3},
+			value2:    []int{1, 3, 2},
+			wantError: true,
+			wantMessage: `ERROR: [1] got: "2", want: "3"/` +
+				`ERROR: [2] got: "3", want: "2"`,
+		},
+		{
+			name:        "empty a list",
+			value1:      []int{},
+			value2:      []int{1, 2, 3},
+			wantError:   true,
+			wantMessage: `ERROR: length: got 0, want: 3`,
+		},
+		{
+			name:        "empty b list",
+			value1:      []int{1, 2, 3},
+			value2:      []int{},
+			wantError:   true,
+			wantMessage: `ERROR: length: got 3, want: 0`,
+		},
+		{
+			name:        "nil a list",
+			value2:      []int{1, 2, 3},
+			wantError:   true,
+			wantMessage: `ERROR: length: got 0, want: 3`,
+		},
+		{
+			name:        "nil b list",
+			value1:      []int{1, 2, 3},
+			wantError:   true,
+			wantMessage: `ERROR: length: got 3, want: 0`,
+		},
+	}
+
+	for _, c := range candidates {
+		t.Run(c.name, func(t *testing.T) {
+			ts := &TestSpy{}
+			assert.EqualList(ts, c.value1, c.value2)
+			if c.wantError {
+				if !ts.ErrorCalled {
+					t.Errorf("ERROR: error not detected")
+				}
+				if ts.ErrorMessage != c.wantMessage {
+					t.Errorf("ERROR: got: \"%s\", want: \"%s\"",
 						ts.ErrorMessage, c.wantMessage)
 				}
 			}
