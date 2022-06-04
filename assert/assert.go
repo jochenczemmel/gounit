3991b3afc3b95351f59057fedf5e95af0b2bfd4c
jochen <jochen.czemmel@web.de>
2022-06-04 22:30:20 +0200

    new functions '*Fail'
    messages in constants

--- assert/assert.go
+++ assert/assert.go
@@ -7,24 +7,35 @@ type ErrorHelper interface {
 	Errorf(string, ...any)
 }
 
+// FailHelper enables using the functions with testing.T,
+// testing.B and testing.F and for unit testing.
+type FailHelper interface {
+	Helper()
+	Failf(string, ...any)
+}
+
 // Equal checks if two values are equal. It calls e.Errorf() if not.
 func Equal[C comparable](e ErrorHelper, got, want C) {
-
 	e.Helper()
-
 	if got != want {
-		e.Errorf("ERROR: got: \"%v\", want: \"%v\"", got, want)
+		e.Errorf(msgGotWant, got, want)
+	}
+}
+
+// Equal checks if two values are equal. It calls f.Failf() if not.
+func EqualFail[C comparable](f FailHelper, got, want C) {
+	f.Helper()
+	if got != want {
+		f.Failf(msgGotWant, got, want)
 	}
 }
 
 // NotEqual checks if two values are not equal, otherwise
 // it calls e.Errorf().
 func NotEqual[C comparable](e ErrorHelper, got, want C) {
-
 	e.Helper()
-
 	if got == want {
-		e.Errorf("ERROR: got: \"%v\", want something unequal", got)
+		e.Errorf(msgWantUnequal, got)
 	}
 }
 
@@ -35,14 +46,13 @@ func EqualList[L ~[]C, C comparable](e ErrorHelper, got, want L) {
 	e.Helper()
 
 	if len(got) != len(want) {
-		e.Errorf("ERROR: length: got: %d, want: %d",
-			len(got), len(want))
+		e.Errorf(msgLengthGotWant, len(got), len(want))
 		return
 	}
 
 	for i, w := range want {
 		if got[i] != w {
-			e.Errorf("ERROR: [%d] got: \"%v\", want: \"%v\"", i, got[i], w)
+			e.Errorf(msgSliceMapGotWant, i, got[i], w)
 		}
 	}
 }
@@ -55,18 +65,18 @@ func EqualMap[M ~map[K]V, K, V comparable](e ErrorHelper,
 	e.Helper()
 
 	if len(got) != len(want) {
-		e.Errorf("ERROR: length: got: %d, want: %d", len(got), len(want))
+		e.Errorf(msgLengthGotWant, len(got), len(want))
 		return
 	}
 
 	for k, w := range want {
 		g, ok := got[k]
 		if !ok {
-			e.Errorf("ERROR: wanted key missing: \"%v\"", k)
+			e.Errorf(msgKeyMissing, k)
 			continue
 		}
 		if g != w {
-			e.Errorf("ERROR: [%v]: got: \"%v\", want: \"%v\"", k, g, w)
+			e.Errorf(msgSliceMapGotWant, k, g, w)
 		}
 	}
 }
@@ -74,9 +84,7 @@ func EqualMap[M ~map[K]V, K, V comparable](e ErrorHelper,
 // Error calls e.Errorf() if an error ist wanted and not received
 // or if an error is not wanted but received.
 func Error(e ErrorHelper, err error, want bool) {
-
 	e.Helper()
-
 	if want {
 		IsError(e, err)
 		return
@@ -89,7 +97,7 @@ func Error(e ErrorHelper, err error, want bool) {
 func IsError(e ErrorHelper, err error) {
 	e.Helper()
 	if err == nil {
-		e.Errorf("ERROR: error not detected")
+		e.Errorf(msgErrorNotDetected)
 	}
 }
 
@@ -98,6 +106,45 @@ func IsError(e ErrorHelper, err error) {
 func NoError(e ErrorHelper, err error) {
 	e.Helper()
 	if err != nil {
-		e.Errorf("ERROR: unexpected error: \"%v\"", err)
+		e.Errorf(msgUnexpectedError, err)
 	}
 }
+
+// ErrorFail calls e.Failf() if an error ist wanted and not received
+// or if an error is not wanted but received.
+func ErrorFail(f FailHelper, err error, want bool) {
+	f.Helper()
+	if want {
+		IsErrorFail(f, err)
+		return
+	}
+	NoErrorFail(f, err)
+}
+
+// IsErrorFail calls f.Failf() if err contains no error.
+// In table tests, use function 'ErrorFail' with boolean values.
+func IsErrorFail(f FailHelper, err error) {
+	f.Helper()
+	if err == nil {
+		f.Failf(msgErrorNotDetected)
+	}
+}
+
+// NoErrorFail calls f.Failf() if err contains an error.
+// In table tests, use function 'ErrorFail' with boolean values.
+func NoErrorFail(f FailHelper, err error) {
+	f.Helper()
+	if err != nil {
+		f.Failf(msgUnexpectedError, err)
+	}
+}
+
+const (
+	msgErrorNotDetected = "ERROR: error not detected"
+	msgUnexpectedError  = "ERROR: unexpected error: \"%v\""
+	msgGotWant          = "ERROR: got: \"%v\", want: \"%v\""
+	msgLengthGotWant    = "ERROR: length: got: %d, want: %d"
+	msgSliceMapGotWant  = "ERROR: [%v]: got: \"%v\", want: \"%v\""
+	msgWantUnequal      = "ERROR: got: \"%v\", want something unequal"
+	msgKeyMissing       = "ERROR: key missing: \"%v\""
+)
