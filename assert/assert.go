15012d35d640fa16917833adcdd013e3fe822329
jochen <jochen.czemmel@web.de>
2022-06-04 21:22:39 +0200

    new function 'EqualMap'

--- assert/assert.go
+++ assert/assert.go
@@ -9,7 +9,9 @@ type ErrorHelper interface {
 
 // Equal checks if two values are equal. It calls Error if not.
 func Equal[C comparable](e ErrorHelper, got, want C) {
+
 	e.Helper()
+
 	if got != want {
 		e.Errorf("ERROR: got: \"%v\", want: \"%v\"", got, want)
 	}
@@ -18,15 +20,42 @@ func Equal[C comparable](e ErrorHelper, got, want C) {
 // EqualList checks if two lists are equal.
 // If the lenght is different, the elements are not compared.
 func EqualList[L ~[]C, C comparable](e ErrorHelper, got, want L) {
+
 	e.Helper()
+
 	if len(got) != len(want) {
-		e.Errorf("ERROR: length: got %d, want: %d",
+		e.Errorf("ERROR: length: got: %d, want: %d",
 			len(got), len(want))
 		return
 	}
+
 	for i, w := range want {
 		if got[i] != w {
 			e.Errorf("ERROR: [%d] got: \"%v\", want: \"%v\"", i, got[i], w)
+		}
+	}
+}
+
+// EqualMap checks if two maps are equal.
+// If the lenght is different, the elements are not compared.
+func EqualMap[M ~map[K]V, K, V comparable](e ErrorHelper,
+	got, want M) {
+
+	e.Helper()
+
+	if len(got) != len(want) {
+		e.Errorf("ERROR: length: got: %d, want: %d", len(got), len(want))
+		return
+	}
+
+	for k, w := range want {
+		g, ok := got[k]
+		if !ok {
+			e.Errorf("ERROR: wanted key missing: \"%v\"", k)
+			continue
+		}
+		if g != w {
+			e.Errorf("ERROR: [%v]: got: \"%v\", want: \"%v\"", k, g, w)
 		}
 	}
 }
--- assert/assert_test.go
+++ assert/assert_test.go
@@ -65,25 +65,28 @@ func TestEqual(t *testing.T) {
 	}
 }
 
+type intList []int
+
 func TestEqualList(t *testing.T) {
 
 	candidates := []struct {
 		name           string
-		value1, value2 []int
+		value1, value2 intList
 		wantError      bool
 		wantMessage    string
+		// value1, value2 []int
 	}{
 		{
 			name:   "equal",
-			value1: []int{1, 2, 3},
-			value2: []int{1, 2, 3},
+			value1: intList{1, 2, 3},
+			value2: intList{1, 2, 3},
 		},
 		{
 			name:        "different length",
 			value1:      []int{1, 2, 3},
 			value2:      []int{1, 2},
 			wantError:   true,
-			wantMessage: `ERROR: length: got 3, want: 2`,
+			wantMessage: `ERROR: length: got: 3, want: 2`,
 		},
 		{
 			name:        "different value",
@@ -101,30 +104,30 @@ func TestEqualList(t *testing.T) {
 				`ERROR: [2] got: "3", want: "2"`,
 		},
 		{
-			name:        "empty a list",
+			name:        "empty got list",
 			value1:      []int{},
 			value2:      []int{1, 2, 3},
 			wantError:   true,
-			wantMessage: `ERROR: length: got 0, want: 3`,
+			wantMessage: `ERROR: length: got: 0, want: 3`,
 		},
 		{
-			name:        "empty b list",
+			name:        "empty want list",
 			value1:      []int{1, 2, 3},
 			value2:      []int{},
 			wantError:   true,
-			wantMessage: `ERROR: length: got 3, want: 0`,
+			wantMessage: `ERROR: length: got: 3, want: 0`,
 		},
 		{
-			name:        "nil a list",
+			name:        "nil got list",
 			value2:      []int{1, 2, 3},
 			wantError:   true,
-			wantMessage: `ERROR: length: got 0, want: 3`,
+			wantMessage: `ERROR: length: got: 0, want: 3`,
 		},
 		{
-			name:        "nil b list",
+			name:        "nil want list",
 			value1:      []int{1, 2, 3},
 			wantError:   true,
-			wantMessage: `ERROR: length: got 3, want: 0`,
+			wantMessage: `ERROR: length: got: 3, want: 0`,
 		},
 	}
 
@@ -132,6 +135,85 @@ func TestEqualList(t *testing.T) {
 		t.Run(c.name, func(t *testing.T) {
 			ts := &TestSpy{}
 			assert.EqualList(ts, c.value1, c.value2)
+			if c.wantError {
+				if !ts.ErrorCalled {
+					t.Errorf("ERROR: error not detected")
+				}
+				if ts.ErrorMessage != c.wantMessage {
+					t.Errorf("ERROR: got: \"%s\", want: \"%s\"",
+						ts.ErrorMessage, c.wantMessage)
+				}
+			}
+			if !c.wantError && ts.ErrorCalled {
+				t.Errorf("ERROR: false alarm")
+				t.Logf("NOTE: error message is: %v", ts.ErrorMessage)
+			}
+		})
+	}
+}
+
+type intStringMap map[int]string
+
+func TestEqualMap(t *testing.T) {
+
+	candidates := []struct {
+		name           string
+		value1, value2 intStringMap
+		wantError      bool
+		wantMessage    string
+		// value1, value2 map[int]string
+	}{
+		{
+			name:   "equal",
+			value1: intStringMap{1: "one", 2: "two", 3: "three"},
+			value2: intStringMap{1: "one", 2: "two", 3: "three"},
+		},
+		{
+			name:        "different length",
+			value1:      map[int]string{1: "one", 2: "two", 3: "three"},
+			value2:      map[int]string{1: "one", 3: "three"},
+			wantError:   true,
+			wantMessage: `ERROR: length: got: 3, want: 2`,
+		},
+		{
+			name:        "different key",
+			value1:      map[int]string{1: "one", 2: "two", 3: "three"},
+			value2:      map[int]string{1: "one", 4: "two", 3: "three"},
+			wantError:   true,
+			wantMessage: `ERROR: wanted key missing: "4"`,
+		},
+		{
+			name:        "empty value missing key",
+			value1:      map[int]string{1: "one", 2: "two", 3: "three"},
+			value2:      map[int]string{1: "one", 4: "", 3: "three"},
+			wantError:   true,
+			wantMessage: `ERROR: wanted key missing: "4"`,
+		},
+		{
+			name:        "different value",
+			value1:      map[int]string{1: "one", 2: "two", 3: "three"},
+			value2:      map[int]string{1: "ONE", 2: "two", 3: "three"},
+			wantError:   true,
+			wantMessage: `ERROR: [1]: got: "one", want: "ONE"`,
+		},
+		{
+			name:        "nil got map",
+			value2:      map[int]string{1: "one", 2: "two", 3: "three"},
+			wantError:   true,
+			wantMessage: `ERROR: length: got: 0, want: 3`,
+		},
+		{
+			name:        "nil want map",
+			value1:      map[int]string{1: "one", 2: "two", 3: "three"},
+			wantError:   true,
+			wantMessage: `ERROR: length: got: 3, want: 0`,
+		},
+	}
+
+	for _, c := range candidates {
+		t.Run(c.name, func(t *testing.T) {
+			ts := &TestSpy{}
+			assert.EqualMap(ts, c.value1, c.value2)
 			if c.wantError {
 				if !ts.ErrorCalled {
 					t.Errorf("ERROR: error not detected")
